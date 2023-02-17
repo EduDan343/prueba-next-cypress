a2180b2e5903f31e3302e93f3033a9d1b9a1deb0
Eduardo Gonzalez <dan_999@hotmail.es>
2023-02-17 09:05:34 -0600

    editar prueba cypress

--- cypress/e2e/prueba-next.cy.js
+++ cypress/e2e/prueba-next.cy.js
@@ -7,6 +7,6 @@ describe('template spec', () => {
         console.log('data :', $data[0].innerText)
         expect($data[0].innerText).to.have.length.greaterThan(20)
       })
-      cy.get('h3').should('be.visible').contains("DAN" && "MEXICO")
+      cy.get('h3').should('be.visible').contains("DAN" && "FRANCIA")
   })
 })
